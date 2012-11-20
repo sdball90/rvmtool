b57eac53cfc38c9cb40efcb1ae0c3d91cd76e02c
Dennis Magee <dmmagee@unm.edu>
2012-11-19 20:22:28 -0700

    Speed boost for sqlite

--- xls2db.m
+++ xls2db.m
@@ -4,12 +4,15 @@ function test = xls2db(file)
 %
 %
 cmd = '';
-sqlite('drop table t','test.sqlite3');
+conn = sqliteopen('test.db');
+sqlitecmd(conn,'drop table t');
 [raw,raw,raw] = xlsread(file);
 [length,width] = size(raw);
-sqlite('create table t(tblid integer primary key)','test.sqlite3');
+sqlitecmd(conn,'create table t(tblid integer primary key)');
 for i = 1:width
     cmd = 'alter table t add ';
     cmd = [cmd,regexprep(char(raw(1,i)),'[^\w'']','')];
-    [test,test] = sqlite(char(cmd),'test.sqlite3');
+    [test,test] = sqlitecmd(conn,cmd);
 end
+sqlitecmd(conn,'commit');
+sqliteclose(conn);
