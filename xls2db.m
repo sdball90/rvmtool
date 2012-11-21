118ee1bc62eda19fe0be94d742257800c1214293
Dennis Magee <dmmagee@unm.edu>
2012-11-20 20:27:18 -0700

    Added insert xls data to database

--- xls2db.m
+++ xls2db.m
@@ -9,10 +9,31 @@ sqlitecmd(conn,'drop table t');
 [raw,raw,raw] = xlsread(file);
 [length,width] = size(raw);
 sqlitecmd(conn,'create table t(tblid integer primary key)');
+sqlitecmd(conn,'commit');
 for i = 1:width
-    cmd = 'alter table t add ';
-    cmd = [cmd,regexprep(char(raw(1,i)),'[^\w'']','')];
+    cmd = sprintf('alter table t add ''%s'';',char(raw(1,i)));
     [test,test] = sqlitecmd(conn,cmd);
 end
+sqlitecmd(conn,'commit');
+
+for i = 2:length
+    cmd = sprintf('insert into t (tblid) values (%i)',i-1);
+    [test,test] = sqlitecmd(conn,cmd);
+    sqlitecmd(conn,'commit');
+    for j = 1:width
+        if (isnan(cell2mat(raw(i,j)))==1)
+            data = '';
+        elseif (iscellstr(raw(i,j))==1)
+            data = sprintf('%s',char(raw(i,j)));
+        else
+            data = sprintf('%d',cell2mat(raw(i,j)));
+        end
+        index = sprintf('%s',char(raw(1,j)));
+        data = strrep(data,'''','');
+        cmd = sprintf('update t set ''%s''=''%s'' where tblid=%i',index,data,i-1);
+        [test,test] = sqlitecmd(conn,cmd);
+    end
+end
+
 sqlitecmd(conn,'commit');
 sqliteclose(conn);
