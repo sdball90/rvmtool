9e99e73949be7cf34409fc905962f4f310d622f8
Dennis Magee <dmmagee@unm.edu>
2012-12-17 11:43:29 -0700

    Started relationship finding

--- rfind.m
+++ rfind.m
@@ -11,8 +11,15 @@ dbid = sqliteopen('test.db');
 for i = 2:colnum
    cmd = sprintf('select "%s" from t',char(column_name(1,i)));
    data = sqlitecmd(dbid,cmd);
-   for j = 1:rownum
-       
+   for j = 1:(rownum-1)
+	   string = regexp(char(data(j)),',','split');
+	   [~,length] = size(string);
+	   for k = 1:length
+		   look = strtrim(string(k));
+		   look = strrep(look,'"','""');
+           cmd = sprintf('select tblid from t where "%s" like "%%%s%%"', char(column_name(1,i)), char(look));
+		   result = sqlitecmd(dbid,cmd);
+       end
    end
 end
 
