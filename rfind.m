084f8d0e0bc3a7d85bf56880bd777315b359dedb
Dennis Magee <dmmagee@unm.edu>
2012-12-18 19:55:56 -0700

    Fixed storage of numbers into database

--- rfind.m
+++ rfind.m
@@ -9,17 +9,25 @@ dbid = sqliteopen('test.db');
 [~,colnum] = size(column_name);
 
 for i = 2:colnum
-   cmd = sprintf('select "%s" from t',char(column_name(1,i)));
-   data = sqlitecmd(dbid,cmd);
-   for j = 1:(rownum-1)
-	   string = regexp(char(data(j)),',','split');
-	   [~,length] = size(string);
-	   for k = 1:length
-		   look = strtrim(string(k));
-		   look = strrep(look,'"','""');
-           cmd = sprintf('select tblid from t where "%s" like "%%%s%%"', char(column_name(1,i)), char(look));
-		   result = sqlitecmd(dbid,cmd);
-       end
+	cmd = sprintf('select "%s" from t',char(column_name(1,i)));
+	data = sqlitecmd(dbid,cmd);
+	for j = 1:rownum
+		if iscellstr(data(j))
+			if isempty(char(data(j)))
+				continue;
+			end
+			string = regexp(char(data(j)),',','split');
+			[~,length] = size(string);
+			for k = 1:length
+				look = strtrim(string(k));
+				look = strrep(look,'''','''''');
+				cmd = sprintf('like ''%%%s%%''', char(look));
+    		end
+		else
+			cmd = sprintf('= %d',cell2mat(data(j)));
+		end
+		cmd = sprintf('select tblid from t where "%s" %s', char(column_name(1,i)), cmd)
+		result = sqlitecmd(dbid,cmd)
    end
 end
 
